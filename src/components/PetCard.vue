5a2f3eecbc73986681222232f48eeb18f9f59f91
allenhong <s890257s@yahoo.com.tw>
2025-03-28 00:33:25 +0800

    寫一點點

---
+++ src/components/PetCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card class="ml-3 mr-3 mt-3 mb-3">
+    <template #title>
+      <h1 class="text-center">123</h1>
+    </template>
+
+    <template #text>
+      <div class="align-center justify-center d-flex w-100">
+        <img src="https://fakeimg.pl/300x300" class="w-75" />
+      </div>
+    </template>
+
+    <v-card-actions class="align-center justify-between">
+      <div class="w-100 ml-10">
+        <h3>1</h3>
+      </div>
+
+      <div class="w-100">
+        <v-btn>
+          <v-icon icon="mdi-emoticon-poop" size="x-large" />
+        </v-btn>
+
+        <v-btn>
+          <v-icon icon="mdi-heart" size="x-large" color="red" />
+        </v-btn>
+      </div>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- src/views/PetView.vue
+++ src/views/PetView.vue
@@ -1,9 +1,56 @@
 <template>
   <div>
-    <h1>這裡是寵物頁面啦</h1>
+    <v-container class="bg-surface-variant mt-10">
+      <v-row no-gutters>
+        <v-col v-for="p in pets" :key="p.id" cols="12" sm="4">
+          <PetCard></PetCard>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import PetCard from "@/components/PetCard.vue";
+
+const pets = ref([
+  {
+    id: 1,
+    name: "MAX",
+    age: 3,
+    image: "https://fakeimg.pl/200x200/?text=MAX",
+    owner: "Alice",
+  },
+  {
+    id: 2,
+    name: "LUNA",
+    age: 1,
+    image: "https://fakeimg.pl/200x200/?text=LUNA",
+    owner: "Bob",
+  },
+  {
+    id: 3,
+    name: "CHARLIE",
+    age: 5,
+    image: "https://fakeimg.pl/200x200/?text=CHARLIE",
+    owner: "Charlie",
+  },
+  {
+    id: 3,
+    name: "CHARLIE",
+    age: 5,
+    image: "https://fakeimg.pl/200x200/?text=CHARLIE",
+    owner: "Charlie",
+  },
+  {
+    id: 3,
+    name: "CHARLIE",
+    age: 5,
+    image: "https://fakeimg.pl/200x200/?text=CHARLIE",
+    owner: "Charlie",
+  },
+]);
+</script>
 
 <style scoped></style>
